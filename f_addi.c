8b6794b1f3bb9e383fb5db527e51a4bf63e4f68e
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2021-01-25 17:48:10 +0100

    f_addi implemented

--- f_addi.c
+++ f_addi.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "types.h"
 #include "mem_abs.h"
 
@@ -9,8 +8,19 @@ void F_ADDI(void)
 	// tyle jedynek, jaka jest dl. zmiennej
 	// przesuniecie bitowe >>20 bo pierwszy bit na 20-tym miejscu
 	writeOpcode();
+	// 5-bit
 	DataType RS1=(getMEMC(getPC()) & 0xF8000) >>15;
+	// 12-bit
 	DataType IMM12=(getMEMC(getPC()) & 0xFFF00000) >>20;
+	// 5-bit
 	DataType RD=(getMEMC(getPC()) & 0xF80) >>7;
 
+	if(getRegister(RS1) + getRegister(IMM12) > MAX_DATA_VAL)
+        setFlagsRegister(FLAG_C);
+    else
+        resetFlagsRegister(FLAG_C);
+        
+    setRegister(RD, (getRegister(RS1) + getRegister(IMM12)) % MAX_DATA_VAL);
+
+    incPC();
 }
