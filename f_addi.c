bc9c6ba46964a473baa9e7f8d141d7e17feefa8a
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2021-01-25 15:53:33 +0100

    draft f_addi

--- f_addi.c
+++ f_addi.c
@@ -3,6 +3,14 @@
 #include "types.h"
 #include "mem_abs.h"
 
-void F_ADDI(void){
-	writeOpcode();	
+void F_ADDI(void)
+{
+	// maska 0xFFF00000 - jedynki na bitach, na których nam zalezy
+	// tyle jedynek, jaka jest dl. zmiennej
+	// przesuniecie bitowe >>20 bo pierwszy bit na 20-tym miejscu
+	writeOpcode();
+	DataType RS1=(getMEMC(getPC()) & 0xF8000) >>15;
+	DataType IMM12=(getMEMC(getPC()) & 0xFFF00000) >>20;
+	DataType RD=(getMEMC(getPC()) & 0xF80) >>7;
+
 }
