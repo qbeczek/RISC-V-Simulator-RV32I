b980f98efa64b05fde26bb6f17c6fbd9ebd0c7fc
Mikołaj Grajeta <mikolajgrajeta@MacBook-Pro-Mikoaj-2.local>
2021-01-25 18:55:05 +0100

    sw maski

--- f_sw.c
+++ f_sw.c
@@ -5,4 +5,10 @@
 
 void F_SW(void){
 	writeOpcode();
+    
+    DataType RS1=(getMEMC(getPC()) & 0xF8000) >> 15;
+    DataType RS2=(getMEMC(getPC()) & 0x1F00000) >> 20;
+    DataType imm =((getMEMC(getPC()) & 0xF80) >> 7) + ((getMEMC(getPC()) & 0xFE000000) >> 20);
+    
 }
+
