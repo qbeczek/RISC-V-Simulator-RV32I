b2f3db692b0f1716cd53e39c65738f3d65c26738
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2021-01-26 21:26:27 +0100

    f_sw and f_sb fixed, not tested

--- f_sb.c
+++ f_sb.c
@@ -11,18 +11,14 @@ void F_SB(void)
 	// 5-bit; 20-24
 	DataType RS2 = getRS2(); //stary zapis JonczykPRO (getMEMC(getPC()) & 0xF80000) >>20;
 	
-    // 5-bit; 7-11
-	DataType IMM5=(getMEMC(getPC()) & 0x7C0) >>7;
-	// 7-bit; 25-31
-	DataType IMM7=(getMEMC(getPC()) & 0xFE000000) >> 25;
+	// 5-bit IMM5 + 7-bit IMM7
+	DataType IMM12 = ((getMEMC(getPC()) & 0x7C0) >> 7) + ((getMEMC(getPC()) & 0xFE000000) >> 25);
 
-	DataType IMM12 = (IMM5 + IMM7);
+	// setRegister(RS1, (getRegister(RS2 + (int8_t)IMM12)));
+	// checkR0(RS1);
 
-    setRegister(RS1, (getRegister(RS2 + IMM12)));
-    checkR0(RS1);
-	
-	// IMM12 jako offset
-	printf("0x%04x: SB R%d, IMM12%d(R%d)\n", getPC(), RS1, IMM12, RS2);
-	
+	setMEMD(getRegister(RS1), getRegister((int8_t)IMM12 + (RS2)));
+	// IMM12 jako offset - adres RS2+IMM
+	printf("0x%04x: SB R%d, IMM12%d(R%d)\n", getMEMD(), RS1, IMM12, RS2);
     incPC();
 }
--- f_sw.c
+++ f_sw.c
@@ -10,11 +10,10 @@ void F_SW(void){
     DataType RS2 = getRS2();
     DataType IMM17 =((getMEMC(getPC()) & 0xF80) >> 7) + ((getMEMC(getPC()) & 0xFE000000) >> 20);
     
-    setMEMD(getRegister(RS1) + (int32_t)IMM17, getRegister(RS2));
+    setMEMD(getRegister(RS1), getRegister((int32_t)IMM17 + RS2));
 
     printf("0x%04x: SW R%d, R%d, R%d\n", getMEMD(), RS1, RS2, IMM17);
     // inkrementacja licznika rozkazow
     incPC();
-    
 }
 
