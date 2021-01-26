a5b3407464e8b585ec6d78a0cde94f3de4c386f0
Qbeczek <jakszyma@gmail.com>
2021-01-26 11:01:29 +0100

    commented section in f_jal cause has an error, not compiling

--- f_jal.c
+++ f_jal.c
@@ -5,9 +5,9 @@
 
 int32_t IMM_WORD_ALIGNMENT(DataType BEFORE_WA){  //funkcja do rozszerzenia ze znakiem 
     
-    if(((BEFORE_WA & 0x80000) >> 19) = 1) //jeśli najstarszy bit z 20 bitowego wyrazenia IMM jest jedynka to powiela jedynke, jesli jest zerem to powiela zero od 20 do 32 bitu
-        return 0xFFF00000 + BEFORE_WA; // 0xFFF00000 to 12 jedynek i 20 zer + BEFORE_WA - to wartosci pierwszych 20 bitow
-    else return BEFORE_WA;
+    // if(((BEFORE_WA & 0x80000) >> 19) = 1) //jeśli najstarszy bit z 20 bitowego wyrazenia IMM jest jedynka to powiela jedynke, jesli jest zerem to powiela zero od 20 do 32 bitu
+    //     return 0xFFF00000 + BEFORE_WA; // 0xFFF00000 to 12 jedynek i 20 zer + BEFORE_WA - to wartosci pierwszych 20 bitow
+    // else return BEFORE_WA;
         
 }
 
