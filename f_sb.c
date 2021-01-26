edb36777e1d61391e9f2ad511fb0ebf6a35f8f6b
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2021-01-26 01:08:03 +0100

    f_sb.c zaimplementowana, nie testowana

--- f_sb.c
+++ f_sb.c
@@ -3,6 +3,16 @@
 #include "types.h"
 #include "mem_abs.h"
 
-void F_SB(void){
-	writeOpcode();
+void F_SB(void)
+{
+	// 5-bit; 15-19
+	DataType RS1=(getMEMC(getPC()) & 0x7C000) >>15;
+	// 5-bit; 20-24
+	DataType RS2=(getMEMC(getPC()) & 0xF80000) >>20;
+
+	// zapisz w pamięci pod efektywny adres wartość 8 bitową – używaj młodszych bitów z rs2
+	// nie wiem czy to jest ok
+    setRegister(RS1, (getRegister(RS2)));
+
+    incPC();
 }
