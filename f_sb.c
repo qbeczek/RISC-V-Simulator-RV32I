2331ea9eee007ffd91582269ca57380b8065b70c
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2021-01-26 18:15:25 +0100

    f_sb implemented, not tested yet

--- f_sb.c
+++ f_sb.c
@@ -12,16 +12,16 @@ void F_SB(void)
 	DataType RS2 = getRS2(); //stary zapis JonczykPRO (getMEMC(getPC()) & 0xF80000) >>20;
 	
     // 5-bit; 7-11
-    //do zsumowania oba imm i do poprawy przesuniecie imm7
 	DataType IMM5=(getMEMC(getPC()) & 0x7C0) >>7;
 	// 7-bit; 25-31
-	DataType IMM7=(getMEMC(getPC()) & 0xFE000000) >>7;
+	DataType IMM7=(getMEMC(getPC()) & 0xFE000000) >> 25;
 
-	printf("0x%04x: SB R%d, R%d\n", getPC(), RS1, RS2);
+	DataType IMM12 = (IMM5 + IMM7);
 
-	// zapisz w pamięci pod efektywny adres wartość 8 bitową – używaj młodszych bitów z rs2
-	// tu pewnie musze jeszcze przesunac o ten offset IMM12: IMM5 i IMM7 - moze getRegister(RS2 + IMM12)?
-    setRegister(RS1, (getRegister(RS2))); //tutaj trzeba chyba dodac IMM Grajetny tutaj
+	// IMM12 jako offset
+	printf("0x%04x: SB R%d, R%d(R%d)\n", getPC(), RS1, IMM12, RS2);
+
+    setRegister(RS1, (getRegister(RS2 + IMM12)));
     checkR0(RS1);
     incPC();
 }
