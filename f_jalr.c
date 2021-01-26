6f6ccece6871ecc79d8100dc670cd39d3f817222
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2021-01-26 01:53:29 +0100

    sb implemented, not tested | jalr core draft, not finished

--- f_jalr.c
+++ f_jalr.c
@@ -5,4 +5,17 @@
 
 void F_JALR(void){
 	writeOpcode();
+
+	// 5-bit; 7-11
+	DataType RD=(getMEMC(getPC()) & 0xF80) >>7;
+	// 5-bit; 15-19
+	DataType RS1=(getMEMC(getPC()) & 0xF8000) >>15;
+	// 12-bit; 20-31
+	DataType IMM12=(getMEMC(getPC()) & 0xFFF00000) >>20;
+	
+	printf("0x%04x: JALR R%d, R%d, R%d\n", getPC(), RD, RS1, IMM12);
+        
+    //TODO: implement function
+
+    incPC();
 }
--- f_sb.c
+++ f_sb.c
@@ -9,9 +9,15 @@ void F_SB(void)
 	DataType RS1=(getMEMC(getPC()) & 0x7C000) >>15;
 	// 5-bit; 20-24
 	DataType RS2=(getMEMC(getPC()) & 0xF80000) >>20;
+	// 5-bit; 7-11
+	DataType IMM5=(getMEMC(getPC()) & 0x7C0) >>7;
+	// 7-bit; 25-31
+	DataType IMM7=(getMEMC(getPC()) & 0xFE000000) >>7;
+
+	printf("0x%04x: SB R%d, R%d\n", getPC(), RS1, RS2);
 
 	// zapisz w pamięci pod efektywny adres wartość 8 bitową – używaj młodszych bitów z rs2
-	// nie wiem czy to jest ok
+	// tu pewnie musze jeszcze przesunac o ten offset IMM12: IMM5 i IMM7 - moze getRegister(RS2 + IMM12)?
     setRegister(RS1, (getRegister(RS2)));
 
     incPC();
