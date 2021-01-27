aa089b33bae5af92e6f52374dda665f78f9abf27
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2021-01-27 01:05:13 +0100

    auipc IMM value corrected 12 to 20 bits

--- f_auipc.c
+++ f_auipc.c
@@ -6,11 +6,11 @@
 void F_AUIPC(void){
 	//writeOpcode();
 	DataType RD = getRD();
-	DataType IMM12=(getMEMC(getPC()) & 0xFFFFF000) >>12;
+	DataType IMM20=(getMEMC(getPC()) & 0xFFFFF000) >>12;
 
-	printf("0x%04x: AUIPC R%d, R%d\n", getPC(), RD, IMM12);
+	printf("0x%04x: AUIPC R%d, R%d\n", getPC(), RD, IMM20);
         
-    setRegister(RD, (getPC() + (IMM12 << 12)));
+    setRegister(RD, (getPC() + (IMM20 << 12)));
     checkR0(RD);
     incPC();
 }
