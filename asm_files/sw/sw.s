c655fc504b3e5b6547972062e7b6269442da22b8
Qbeczek <jakszyma@gmail.com>
2021-01-27 12:53:50 +0100

    sw last test

--- asm_files/sw/sw.s
+++ asm_files/sw/sw.s
@@ -2,17 +2,15 @@
          .align	4
          .global _start
 _start:  
-		li x1, 0x00000001
-		li x2, 0x00000002
-		li x3, 0x12DEF001
-		li x4, 0xEFFF200F
-		sw x0, 0(x2) #test 0
-		sw x5, 0(x1)
-		sw x6, 0(x2)
-		sw x7, 0(x3)
-		sw x8, 0(x4)
-		sw x9, 1(x1)
-		sw x10, 3(x3)
-		sw x18, 5(x4)
-		sw x29, 2(x4)
-		sw x31, 1(x2)
+		li x1, 0x001
+		li x2, 0x002
+		li x3, 0x003
+		li x4, 0x004
+		sw x1, 0(x0) #test Zero
+		sw x0, 0(x2) 
+		sw x1, 4(x3)
+		sw x2, 1(x5)
+		sw x4, 28(x2)
+		li x5, 0XFFFFFFF
+		sw x5, 10(x5)
+		sw x3, 31(x3)
--- f_sw.c
+++ f_sw.c
@@ -11,10 +11,10 @@ void F_SW(void){
     DataType IMM17 =((getMEMC(getPC()) & 0xF80) >> 7) + ((getMEMC(getPC()) & 0xFE000000) >> 20);
     
     // store value from RS1 to (R2 + IMM)
-    setRegister((RS2) + (int32_t)IMM17, getRegister(RS1));
+    setRegister((RS1) + (int32_t)IMM17, getRegister(RS2));
 
     printf("0x%04x: SW R%d, R%d, R%d\n", getPC(), RS1, RS2, IMM17);
-    checkR0((RS2) + (int32_t)IMM17);
+    checkR0((RS1) + (int32_t)IMM17);
     // Increment Program Counter
     incPC();
 }
