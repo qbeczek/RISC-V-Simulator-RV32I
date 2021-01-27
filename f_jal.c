6af9cbb9444fe5aa7c11afdd67f68e1f39961afb
Mikołaj Grajeta <mikolajgrajeta@MacBook-Pro-Mikoaj-2.local>
2021-01-27 02:04:45 +0100

    JAL zrobiony debesta - uwzglednione zero domniemane

--- f_jal.c
+++ f_jal.c
@@ -5,10 +5,9 @@
 
 DataType IMM_WORD_ALIGNMENT_jal(DataType BEFORE_WA){  //funkcja do rozszerzenia ze znakiem 
     
-     if(((BEFORE_WA & 0x80000) >> 19) == 1) //jeli najstarszy bit z 20 bitowego wyrazenia IMM jest jedynka to powiela jedynke, jesli jest zerem to powiela zero od 20 do 32 bitu
-         return 0xFFF00000 + BEFORE_WA; // 0xFFF00000 to 12 jedynek i 20 zer + BEFORE_WA - to wartosci pierwszych 20 bitow
+     if(((BEFORE_WA & 0x100000) >> 20) == 1) //jeli najstarszy bit z 21 bitowego wyrazenia IMM jest jedynka to powiela jedynke, jesli jest zerem to powiela zero od 20 do 32 bitu
+         return 0xFFE00000 + BEFORE_WA; // 0xFFF00000 to 12 jedynek i 20 zer + BEFORE_WA - to wartosci pierwszych 20 bitow
      else return BEFORE_WA;
-        
 }
 
 void F_JAL(void){
@@ -16,22 +15,16 @@ void F_JAL(void){
     
     DataType RD = getRD();
     
-    //składowe IMM - juz przesuniete, jeszcze nie dodane
-    //DataType IMM_10_1 =  (getMEMC(getPC()) & 0x7FE00000) >> 21;
-    //DataType IMM_11_ =  (getMEMC(getPC()) & 0x100000) >> 10;
-    //DataType IMM_19_12 =  (getMEMC(getPC()) & 0xFF000) >> 1;
-    //DataType IMM_20_ =  (getMEMC(getPC()) & 0x80000000) >> 12;
+    //składowe IMM - bit zerowy IMM jest domniemany i zostawiamy tam wartosc 0
+    //DataType IMM_10_1 =  (getMEMC(getPC()) & 0x7FE00000) >> 20;
+    //DataType IMM_11_ =  (getMEMC(getPC()) & 0x100000) >> 9;
+    //DataType IMM_19_12 =  (getMEMC(getPC()) & 0xFF000);
+    //DataType IMM_20_ =  (getMEMC(getPC()) & 0x80000000) >> 11;
     
-    
-    //do przesuniecia ze wzgledu na bit 0 rowny 0 AAAA
-    DataType IMM20 = ((getMEMC(getPC()) & 0x7FE00000) >> 21) + ((getMEMC(getPC()) & 0x100000) >> 10) + ((getMEMC(getPC()) & 0xFF000) >> 1) + ((getMEMC(getPC()) & 0x80000000) >> 12);
+    DataType IMM20 = ((getMEMC(getPC()) & 0x7FE00000) >> 20) + ((getMEMC(getPC()) & 0x100000) >> 9) + (getMEMC(getPC()) & 0xFF000) + ((getMEMC(getPC()) & 0x80000000) >> 11);
     
     setRegister(RD, getPC() + 4);
     checkR0(RD);
     setPC(IMM_WORD_ALIGNMENT_jal(IMM20) + getPC());
     printf("wynik JAL   RD%d    IMM20%d",RD ,IMM20 );
-    
-    
-    //poprawic bit 0 na 0 imm
-    //tu robie dalej MG
 }
