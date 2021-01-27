37aedb136407fe01d34588866558cd2cc2622fd3
Qbeczek <jakszyma@gmail.com>
2021-01-27 13:32:54 +0100

    przygotowanie do zaliczenia

--- asm_files/risc.sh
+++ asm_files/risc.sh
@@ -7,5 +7,5 @@ FILE=$1
 /home/jszyman/risc-v/bin/riscv64-unknown-elf-objcopy -j ".data" -O binary test file_data_in.bin
 /home/jszyman/risc-v/bin/riscv64-unknown-elf-readelf -a test >test_elf.lst
 
-mv /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/asm_files/file_code.bin /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/test/
-mv /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/asm_files/file_data_in.bin /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/test/
+mv /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/asm_files/file_code.bin /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/sources/test
+mv /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/asm_files/file_data_in.bin /home/jszyman/Desktop/syko/projekt_C/syko/Szymanski_Jakub/szkielet/glowny/sources/test
